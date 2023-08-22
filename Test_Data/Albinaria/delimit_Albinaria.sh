1984952cf6c27e26ae2bf2641bb6763ccc1d6acc
dkornai <danielkornai@gmail.com>
2023-08-22 15:21:30 +0200

    removed albinaria

--- Test_Data/Albinaria/delimit_Albinaria.sh
+++
@@ -1,7 +0,0 @@
-# clear previous results
-rm -rf res_albinaria_split
-rm -rf res_albinaria_merge
-
-# run new analyses
-HMDelimit --cfile mcf_albinaria_merge.txt --cfpor burnin = 50000, nsample = 50000
-HMDelimit --cfile mcf_albinaria_split.txt --cfpor burnin = 50000, nsample = 50000
--- Test_Data/Albinaria/delimit_Albinaria_fast.sh
+++
@@ -1,7 +0,0 @@
-# clear previous results
-rm -rf res_albinaria_split
-rm -rf res_albinaria_merge
-
-# run new analyses
-HMDelimit --cfile mcf_albinaria_merge.txt
-HMDelimit --cfile mcf_albinaria_split.txt
